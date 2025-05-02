388586aaa302de44d8d137bac54d286054f063d3
Ali salhab <alisalhab258@gmail.com>
2025-05-02 16:18:59 +0300

    development branch updated

--- config/app_version.php
+++ config/app_version.php
@@ -4,3 +4,5 @@ return [
     'current_version' => '1.0.0', // Define the current version of your app
     'latest_version' => '1.0.1',  // This can be dynamically updated or fetched from a source
 ];
+
+// sssss
